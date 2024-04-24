cc90275c13baa0edca5de2531d118abcde2539d4
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-04-24 09:39:53 +0530

    Refactored and Fixed Import Errors

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,80 +1,11 @@
-import React from 'react';
-import { Parallax } from 'react-parallax';
-import { Typography, Container, Button } from '@mui/material';
+import { Link } from "react-router-dom";
 
-interface BackgroundImageProps {
-  imageUrl: string;
-  headerText: string;
+export default function PageHome() {
+  return <div className="flex p-5 items-center bg-white shadow-xl justify-between">
+    <h1>Hello, World!</h1>
+    <div className="flex gap-4">
+      <Link className="font-medium text-blue-700 hover:text-blue-500" to="/features">Features</Link>
+      <Link className="font-medium text-blue-700 hover:text-blue-500" to="/contact">Contact Us</Link>
+    </div>
+  </div>
 }
-
-const BackgroundImage: React.FC<BackgroundImageProps> = ({
-  imageUrl,
-  headerText,
-}) => {
-  return (
-    <Parallax
-      bgImage={imageUrl}
-      strength={400}
-      style={{
-        height: '150vh',
-        width: '100%',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        // filter: 'brightness(50%)',
-        // opacity: '0.8',
-      }}
-    >
-      <div style={{ height: '100vh' }}>
-        <Container
-          maxWidth="md"
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
-            color: 'white',
-            height: '100%',
-            position: 'relative',
-            zIndex: 1,
-          }}
-        >
-          {/* Typography component for the header text */}
-          <Typography variant="h3" component="h1" gutterBottom style={{ opacity: 1.6, transform: 'translateY(30%)' }}>
-            {headerText}
-          </Typography>
-          {/* Typography component for the additional text */}
-          <Typography variant="h2" gutterBottom sx={{
-        color: 'white',
-        fontWeight: 'bolder',
-        textAlign: 'center',
-        fontFamily: 'Inter, sans-serif',
-        marginBottom : '150px',
-      }}>
-        Experience community living at its best
-      </Typography>
-          {/* Button component for the login call-to-action */}
-          <Button
-  variant="contained"
-  color="primary"
-  size="large"
-  sx={{ 
-    position: 'absolute', 
-    right: '150px', 
-    bottom: '208px', 
-    bgcolor: '#323EDD', 
-    color: 'white',
-    whiteSpace: 'nowrap' // Set white-space to nowrap
-  }}
-  href="/login"
->
-  JOIN US
-</Button>
-         
-        </Container>
-      </div>
-    </Parallax>
-  );
-};
-
-export default BackgroundImage;
