e25b7c50b3fcabbddab00bf7699593779791e23f
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-05-06 10:13:30 +0530

    Added Auth

--- server/src/app.js
+++ server/src/app.js
@@ -8,11 +8,12 @@ import helpDeskRoutes from "./routes/helpDeskRoutes.js";
 const { port } = constants;
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/users', userRoutes);
-app.use('/api/helpdesk', helpDeskRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/helpdesk", helpDeskRoutes);
 
 connectDb();
 
@@ -20,9 +21,9 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/hello-world" , (req,res) => {
-  res.json("Hello World !")
-})
+app.get("/hello-world", (req, res) => {
+  res.json("Hello World !");
+});
 
 app.listen(port, () => {
   return console.log(
