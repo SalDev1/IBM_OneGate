0c5e67636848c758db52e9570d99b91400ee05e7
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2024-05-06 15:47:46 +0530

    Added Amenities Time-slot Page

--- src/pages/AmenitiesTimeSlot.tsx
+++ src/pages/AmenitiesTimeSlot.tsx
@@ -2,10 +2,34 @@ import { Card, Grid, Typography } from "@mui/material";
 import DNavBar from "../components/DNavBar";
 import DSideBar from "../components/DSideBar";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import _ from "lodash";
+import { Button } from "@mui/material";
+import ThemedButton from "../components/button";
+
+const bookedSlotsInitial = [
+  {
+    from: 12,
+    to: 14,
+  },
+  {
+    from: 18,
+    to: 19,
+  },
+  {
+    from: 10,
+    to: 11,
+  },
+];
 
 export default function AmenitiesTimeSlot() {
   const location = useLocation();
   const { amenity } = location.state;
+
+  const [bookedTimeSlots, setBookedTimeSlots] = useState(bookedSlotsInitial);
+  const [selectedTimeSlot, setSelectedTimeSlot] = useState<
+    number | undefined
+  >();
 
   return (
     <div style={{ backgroundColor: "#F6F5F2" }}>
@@ -50,6 +74,49 @@ export default function AmenitiesTimeSlot() {
                 <Typography fontWeight="bold" color="grey">
                   Booking Count
                 </Typography>
+                {selectedTimeSlot !== undefined && (
+                  <Typography
+                    sx={{ color: "#0022Ff" }}
+                    fontWeight="bold"
+                    color="grey"
+                  >
+                    Selected Time Slot: {selectedTimeSlot}:00 to{" "}
+                    {selectedTimeSlot + 1}:00
+                  </Typography>
+                )}
+                <Button variant="contained" className="mt-4">
+                  Continue
+                </Button>
+              </Grid>
+              <Grid padding={4}>
+                {_.range(0, 23).map((time) => {
+                  let isBooked = false;
+                  bookedTimeSlots.forEach((slot) => {
+                    if (time >= slot.from && time < slot.to) isBooked = true;
+                  });
+                  return (
+                    <ThemedButton
+                      variant={
+                        isBooked
+                          ? "inactive"
+                          : selectedTimeSlot == time
+                          ? "primary"
+                          : "secondary"
+                      }
+                      className={`mb-4 ${
+                        isBooked ? "bg-red-50" : "hover:scale-110"
+                      }`}
+                      onClick={() => {
+                        if (!isBooked) setSelectedTimeSlot(time);
+                      }}
+                    >
+                      {time}:00 to {time + 1}:00{" "}
+                      {isBooked && (
+                        <span className="font-bold mx-2">Unavailable</span>
+                      )}
+                    </ThemedButton>
+                  );
+                })}
               </Grid>
             </Grid>
           </Card>
